b5adac683b9732f003d4d0c4c91b346c4e5812d7
Gracelyn Prajna <gracelynprajna@gmail.com>
2023-01-09 02:23:59 -0500

    update

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -4,7 +4,8 @@
 //Link is a component that enables us to move between pages without having to do manual refreshes. a tags would reload the whole app while Link only transitions us between the routes.
 //Outlet is what helps display the routes that are listed in the children array when we defined our routes.
 
-import { useLoaderData, Link, useSubmit, Outlet } from "react-router-dom";
+import { useLoaderData, Link, useSubmit, Outlet, useNavigate } from "react-router-dom";
+import { useState } from "react"; 
 import "./App.css";
 
 function TodosPage() {
@@ -41,6 +42,35 @@ function TodoPage() {
 }
 
 function LoginPage() {
-  return <div>Login page</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const login = () => {
+    if(username === "user123" && password === "pass123"){
+      localStorage.setItem("authed", true);
+
+      navigate("/todos");
+    } else {
+        console.log("Username and Password is wrong.");
+      }
+    }
+
+  return(
+    <div>
+      <form onSubmit={login}>
+        <input type="text"
+          onChange={(event) => setUsername(event.target.value)}
+          name="username"
+      />
+        <input type="password"
+          onChange={(event) => setPassword(event.target.value)}
+          name="password"
+      />
+      <input type="submit" value="Login"/>
+      </form>
+    </div>
+  );
 }
 export { TodosPage, TodoPage, LoginPage };
